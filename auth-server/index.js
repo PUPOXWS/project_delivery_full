a23e6025da4b58882365d7bf74a77ef2834d818b
PUPOXWS <pupoxws@gmail.com>
2024-08-05 21:31:18 -0500

    Se agrega conexión con mongo

---
+++ auth-server/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const app = express();
+
+dotenv.config();
+const port = process.env.PORT || 8000;
+app.use(cors());
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB);
+    console.log("Connected to Mongo");
+  } catch (error) {
+    throw error;
+  }
+};
+
+mongoose.connection.on("disconnected", () => {
+  console.log("disconnected");
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("connected");
+});
+
+app.use(express.json({ limit: "3mb" }));
+
+app.listen(port, () => {
+  connect();
+  console.log(`ejecutandose  desde ${port}`);
+});
